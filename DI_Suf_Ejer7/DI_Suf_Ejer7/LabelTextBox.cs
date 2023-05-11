d12ad4e190972089eb4da90388690136582ff801
master <citnas5@gmail.com>
2023-05-11 12:31:13 +0200

    clase

---
+++ DI_Suf_Ejer7/DI_Suf_Ejer7/LabelTextBox.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DI_Suf_Ejer7
+{
+    public partial class LabelTextBox : UserControl
+    {
+        public LabelTextBox()
+        {
+            InitializeComponent();
+        }
+
+        //Indica donde está la etiqueta respecto al TextBox
+        public enum ePosicion
+        {
+            IZQUIERDA, DERECHA
+        }
+
+        private ePosicion posicion = ePosicion.IZQUIERDA;
+        [Category("Appearance")]
+        [Description("Indica si la Label se sitúa a la IZQUIERDA o DERECHA del Textbox")]
+        public ePosicion Posicion
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(ePosicion), value))
+                {
+                    posicion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return posicion;
+            }
+        }
+        
+
+        //Pixeles de separación entre label y textbox
+        private int separacion = 0;
+        [Category("Design")]
+        [Description("Píxels de separación entre Label y Textbox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return separacion;
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Texto asociado a la Label del control")]
+        public string TextLbl
+        {
+            set
+            {
+                lbl.Text = value;
+                recolocar();
+            }
+            get
+            {
+                return lbl.Text;
+            }
+        }
+        [Category("Appearance")]
+        [Description("Texto asociado al TextBox del control")]
+        public string TextTxt
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+        private void recolocar()
+        {
+            switch (posicion)
+            {
+                case ePosicion.IZQUIERDA:
+                    //Establecemos posición del componente lbl
+                    lbl.Location = new Point(0, 0);
+                    // Establecemos posición componente txt
+                    txt.Location = new Point(lbl.Width + Separacion, 0);
+                    //Establecemos ancho del Textbox
+                    //(la label tiene ancho por autosize)
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    //Establecemos altura del componente
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+                case ePosicion.DERECHA:
+                    //Establecemos posición del componente txt
+                    txt.Location = new Point(0, 0);
+                    //Establecemos ancho del Textbox
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    //Establecemos posición del componente lbl
+                    lbl.Location = new Point(txt.Width + Separacion, 0);
+                    //Establecemos altura del componente (Puede sacarse del switch)
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+            }
+        }
+        // Esta función has de enlazarla con el evento SizeChanged.
+        // Sería necesario también tener en cuenta otros eventos como FontChanged
+        // que aquí nos saltamos.
+        private void LabelTextBox_SizeChanged(object sender, EventArgs e)
+        {
+            recolocar();
+        }
+
+        private void txt_KeyUp(object sender, KeyEventArgs e)
+        {
+            lbl.Text = e.KeyData.ToString();
+        }
+
+        [Category("Ejercicio")]
+        [Description("Se lanza cuando el texto cambia")]
+        public event System.EventHandler TxtChanged;
+
+        protected virtual void OnTxtChanged(EventArgs e)
+        {
+            if (TxtChanged != null)
+            {
+                TxtChanged(this, e);
+            }
+        }
+
+    }
+}
